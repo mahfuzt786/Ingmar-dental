c1f7cfe90837a71e53028a7637edfb3f82067411
Syed Mahfuzul Mazid <mahfuzt786@yahoo.co.in>
2021-01-01 12:02:34 +0530

    first file added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Dental RESTfull Intermediary Layer API for Dental
+ * @author Mahfuz <mahfuzz786@gmail.com>
+ * @copyright (c) 2020-21, Alegra Labs <https://www.alegralabs.com>
+ 
+ * The logics uses `regions` of teeth, its `status` and `findings`.
+ * - Returns : subsidy value
+ */
+
+//Include dental Class
+require 'dental.php';
